657e4aac385228ab3d0f64b841e42b2bae17df47
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-10-19 22:24:17 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumber(int numA)
+{
+    if (numA >= 1)
+    {
+        Console.Write($"{numA},");
+        PrintNumber(numA - 1);
+    }
+    else
+    {
+        Console.Write($"{numA}");
+    }
+}
+PrintNumber(numN);
+Console.WriteLine();
