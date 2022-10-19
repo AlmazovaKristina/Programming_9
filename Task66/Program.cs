39a71061b4312632034b8dd3f5841a658ebb4c1b
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-10-19 23:29:32 +0300

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число M:");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N:");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDigits(int numM, int numN)
+{
+    int sum = 0;
+    if (numM < numN)
+    {
+        sum += SumOfDigits(numM+1,numN);
+    }
+    return sum+numM;
+}
+
+int totalAmount = SumOfDigits(numM,numN);
+Console.WriteLine($"{totalAmount}");
+
+
+
